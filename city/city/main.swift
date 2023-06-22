ecbbb8c2ae2f608aa9e82d775034f5b053e6acd9
InfinityLQ <98259150+InfinityLQ@users.noreply.github.com>
2023-06-22 16:43:27 +0300

    Update main.swift by Disctionary  methods

--- city/city/main.swift
+++ city/city/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
+//1:
+var citiesPopulation=["Mogilev":"374713","Brest":"337445","Minsk":"1949070","Gomel":"526901","Grodno":"361445"]
+for (city,population)in citiesPopulation{
+    print("\(city) has population of \(population)")
+}
 
+//2:
+citiesPopulation["Vitebsk"]="373859"
+print("\nAfter adding Vitebsk:")
+for(city,population)in citiesPopulation{
+    print("\(city) has population of \(population)")
+}
